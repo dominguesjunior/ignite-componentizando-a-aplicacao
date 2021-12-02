24cc3671bfa5f2fd8108d0deef50b9e07e0d6734
Dário Junior <dariojunior04@gmail.com>
2021-12-02 08:20:47 -0300

    Criando pequeno componente de Header - Categoria Atual

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { MovieCard } from '../components/MovieCard';
+import { Header } from '../components/Header';
 
 import { api } from '../services/api';
 
@@ -42,9 +43,7 @@ export function Content(props: ContentProps) {
   
   return (
     <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
+      <Header title={selectedGenre.title} />
 
         <main>
           <div className="movies-list">
